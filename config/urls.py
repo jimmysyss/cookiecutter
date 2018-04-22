71f9db92ee763ae638a52d14595b74e469f19e56
Jimmy Wong <tszming.wong@gmail.com>
2018-04-23 01:26:42 +0800

    Add testing project

--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     url(r'^accounts/', include('allauth.urls')),
 
     # Your stuff: custom urls includes go here
-
+    url(r'^finance/', include('finance.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
