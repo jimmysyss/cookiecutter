32b93c52cdad83475f752ebebc270293f682301f
Jimmy Wong <tszming.wong@gmail.com>
2018-05-02 01:13:15 +0800

    Fine tune Currency

--- finance/admin.py
+++ finance/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+from finance.models import Currency
+
+class BaseAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_by', 'created_date', 'modified_by', 'modified_date', )
+
+    def save_model(self, request, obj, form, change):
+        if change:
+            obj.modified_by = request.user.username
+        else:
+            obj.created_by = request.user.username
+            obj.modified_by = request.user.username
+        obj.save()
+
+
+@admin.register(Currency)
+class CurrencyAdmin(BaseAdmin):
+    list_display = ('full_name', 'name',)
--- finance/models.py
+++ finance/models.py
@@ -4,15 +4,25 @@ from django.db import models
 # Create your models here.
 class BaseModel(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
+    created_by = models.CharField(max_length=50, editable=False)
     modified_date = models.DateTimeField(auto_now=True)
+    modified_by = models.CharField(max_length=50, editable=False)
 
     class Meta:
         abstract = True
 
 
 class Currency(BaseModel):
-    pass
+    name = models.CharField(max_length=10)
+    full_name = models.CharField(max_length=50)
+    display_format = models.CharField(max_length=50)
+    flag = models.CharField(max_length=10)
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "currencies"
 
 class CurrencyPair(BaseModel):
     pass
